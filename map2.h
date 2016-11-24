982fd3c5325763776852681637b5dc4fbfcfa344
Jules Lagarde <lagj2703@dinf-tarin.usherbrooke.ca>
2016-11-24 17:35:29 -0500

    la version que j'ai envoye a turnin

--- map2.h
+++ map2.h
@@ -5,11 +5,17 @@
 //  Jean Goulet
 //  Copyleft 2016 UdeS
 //
+// Felix Roy     16 061 351
+// Jules Lagarde 16 140 445
+//
 
 #ifndef map2_h
 #define map2_h
+#include <iostream>
+using namespace std;
 
-#define RATIO 3  // 3:1
+#define RATIO1 3  // 3:1
+#define RATIO2 2
 
 ///////////////////////////////////////////////////////////////////////////
 // lower_bound
@@ -17,7 +23,7 @@
 template <typename Tclef, typename Tvaleur>
 typename map<Tclef,Tvaleur>::iterator map<Tclef,Tvaleur>::lower_bound(const Tclef& c)const{
     noeud* a=RACINE();
-    for(;a->POIDS!=1;){
+    while(a->POIDS!=1){
       if(c < a->CONTENU->first)
         a=a->GAUCHE;
       else if(a->CONTENU->first < c)
@@ -43,9 +49,10 @@ void map<Tclef,Tvaleur>::iterator::avancer(noeud*& p){
     p=p->DROITE;
     while(p->GAUCHE!=nullptr) p=p->GAUCHE;
     return ;
+  }else{
+    while(p->PARENT->DROITE==p && p->PARENT->POIDS!=0) p=p->PARENT;
+    p=p->PARENT;
   }
-  while(p->PARENT->DROITE==p && p->PARENT->POIDS!=0) p=p->PARENT;
-  p=p->PARENT;
 }
 
 template <typename Tclef, typename Tvaleur>
@@ -54,10 +61,10 @@ void map<Tclef,Tvaleur>::iterator::reculer(noeud*& p){
 
     p=p->GAUCHE;
     while(p->DROITE!=nullptr) p=p->DROITE;
-    return ;
+  }else{
+    while(p->PARENT->GAUCHE==p && p->PARENT->POIDS!=0) p=p->PARENT;
+    p=p->PARENT;
   }
-  while(p->PARENT->GAUCHE==p && p->PARENT->POIDS!=0) p=p->PARENT;
-  p=p->PARENT;
 }
 
 
@@ -68,58 +75,66 @@ void map<Tclef,Tvaleur>::iterator::reculer(noeud*& p){
 
 template <typename Tclef, typename Tvaleur>
 void map<Tclef,Tvaleur>::transferer_vers_la_droite(noeud*& p){
+  noeud* gauche=p->GAUCHE;
   size_t poidsDroite=(p->DROITE==nullptr)?0:p->DROITE->POIDS;
   size_t poidsGauche=(p->GAUCHE==nullptr)?0:p->GAUCHE->POIDS;
-  if(p->POIDS>3 && poidsGauche > poidsDroite*RATIO)
+  size_t poidsGDroite=(gauche->DROITE==nullptr)?0:gauche->DROITE->POIDS;
+  size_t poidsGGauche=(gauche->GAUCHE==nullptr)?0:gauche->GAUCHE->POIDS;
+  if(poidsGauche>3 && poidsGDroite > poidsGGauche*RATIO2)
+    rotation_droite_gauche(gauche);
+
+  if(p->POIDS>3 && poidsGauche > poidsDroite*RATIO1)
     rotation_gauche_droite(p);
 }
 
 template <typename Tclef, typename Tvaleur>
 void map<Tclef,Tvaleur>::transferer_vers_la_gauche(noeud*& p){
+  noeud* droite=p->DROITE;
   size_t poidsDroite=(p->DROITE==nullptr)?0:p->DROITE->POIDS;
   size_t poidsGauche=(p->GAUCHE==nullptr)?0:p->GAUCHE->POIDS;
-  if(p->POIDS>3 && poidsDroite > poidsGauche*RATIO)
+  size_t poidsDDroite=(droite->DROITE==nullptr)?0:droite->DROITE->POIDS;
+  size_t poidsDGauche=(droite->GAUCHE==nullptr)?0:droite->GAUCHE->POIDS;
+  if(poidsDroite>3 && poidsDGauche > poidsDDroite*RATIO2)
+    rotation_gauche_droite(droite);
+
+  if(p->POIDS>3 && poidsDroite > poidsGauche*RATIO1)
     rotation_droite_gauche(p);
 }
 
 template <typename Tclef, typename Tvaleur>
-void map<Tclef,Tvaleur>::rotation_gauche_droite(noeud* p){
+void map<Tclef,Tvaleur>::rotation_gauche_droite(noeud*& p){
   noeud* gauche=p->GAUCHE;
   p->GAUCHE=gauche->DROITE;
   if(gauche->DROITE!=nullptr)gauche->DROITE->PARENT=p;
   gauche->DROITE=p;
-  if(p->PARENT->GAUCHE == p)
-    p->PARENT->GAUCHE=gauche;
-  else
-    p->PARENT->DROITE=gauche;
   gauche->PARENT=p->PARENT;
   p->PARENT=gauche;
-
 
   gauche->POIDS=p->POIDS;
   p->POIDS=
     ((p->GAUCHE==nullptr)?0:p->GAUCHE->POIDS)
     + ((p->DROITE==nullptr)?0:p->DROITE->POIDS) +1;
+
+  p=gauche;
 }
 
 template <typename Tclef, typename Tvaleur>
-void map<Tclef,Tvaleur>::rotation_droite_gauche(noeud* p){
+void map<Tclef,Tvaleur>::rotation_droite_gauche(noeud*& p){
   noeud* droite=p->DROITE;
+
+
   p->DROITE=droite->GAUCHE;
   if(droite->GAUCHE!=nullptr)droite->GAUCHE->PARENT=p;
   droite->GAUCHE=p;
-  if(p->PARENT->GAUCHE == p)
-    p->PARENT->GAUCHE=droite;
-  else
-    p->PARENT->DROITE=droite;
   droite->PARENT=p->PARENT;
   p->PARENT=droite;
-
 
   droite->POIDS=p->POIDS;
   p->POIDS=
     ((p->GAUCHE==nullptr)?0:p->GAUCHE->POIDS)
     + ((p->DROITE==nullptr)?0:p->DROITE->POIDS) +1;
+
+  p=droite;
 }
 
 
